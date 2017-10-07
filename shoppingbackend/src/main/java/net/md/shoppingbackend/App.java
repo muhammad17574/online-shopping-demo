6051a2139dcd1ec2a79f229c058707e3dd9f9414
muhammad17574 <muhammadilyas17574@gmail.com>
2017-10-07 10:04:56 +0500

    my project setup completed successfully

---
+++ shoppingbackend/src/main/java/net/md/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.md.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
